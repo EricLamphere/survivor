fa92fd233ba662b22a37f380a5caac0789f874a7
EricLamphere <ericjlamphere@gmail.com>
2024-09-28 17:02:45 -0400

    adding comments

--- app.R
+++ app.R
@@ -1,10 +1,12 @@
 
-
+# load libraries
 library(dplyr)
 library(ezextras)
 library(purrr)
 pkgload::load_all(".")
 
+# load data
 season_picks <- create_season_picks("googlesheets")
 
+# run app
 survivor_app()
