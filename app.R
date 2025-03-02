a39a9c6cbc0f36bba3fdb875b3f6e5c64dc86a0e
EricLamphere <ericjlamphere@gmail.com>
2025-03-01 23:37:13 -0500

    adding verbose log level and adding messages around reading .Renviron file

--- app.R
+++ app.R
@@ -2,10 +2,13 @@
 # R environ file with special environment variables. Right now, this is the only
 # way to deploy from github actions because you can't use the "envVars" parameter
 # in rsconnect::deployApp unless you're using Connect
-renviron_file <- "./.Renviron"
+renviron_file <- ".Renviron"
 if (file.exists(renviron_file)) {
+    cli::cli_alert_warning("Reading variables from .Renviron")
     readRenviron(path = renviron_file)
     file.remove(renviron_file)  
+} else {
+    cli::cli_alert_warning(".Renviron file doesn't exist")
 }
 rm(renviron_file)
 
