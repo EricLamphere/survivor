47cc106ca2309d6366b8d1bbd073315c22642574
EricLamphere <ericjlamphere@gmail.com>
2025-03-01 23:12:08 -0500

    debugging

--- app.R
+++ app.R
@@ -1,4 +1,13 @@
 
+# R environ file with special environment variables. Right now, this is the only
+# way to deploy from github actions because you can't use the "envVars" parameter
+# in rsconnect::deployApp unless you're using Connect
+renviron_file <- "./.Renviron"
+readRenviron(path = renviron_file)
+file.remove(renviron_file)
+rm(renviron_file)
+
+
 # load libraries
 cli::cli_alert_info("Loading libraries")
 library(dplyr)
@@ -13,8 +22,6 @@ pkgload::load_all(".")
 # GLOBALS
 # > I just don't want to refactor everything to take a dataframe input
 counter <<- 0
-
-gs_auth()
 
 all_data <- gs_get_all_data()
 season_picks <<- create_season_picks(all_data = all_data, augment_with_historical = TRUE)
